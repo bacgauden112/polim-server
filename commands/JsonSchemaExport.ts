0adc163de3b06893e073d018de07cb4d0dfb4190
Piggat <nguyenxuankien@alimama.vn>
2017-08-15 09:13:39 +0700

    PMS10: hoàn thiện API tỉ giá, fix các lỗi liên quan integrations trước đó chưa test, sử dụng PolimAccessToken thay cho AccessToken mặc định để phân biệt hai lớp tài khoản, thêm mô tả cấu trúc lỗi vào readme.md, disable bớt remote method ở Customer, schema docs generator

---
+++ commands/JsonSchemaExport.ts
@@ -0,0 +1,80 @@
+/**
+ * Created by Piggat on 8/14/2017.
+ */
+(() => {
+    let argv = require('minimist')(process.argv.slice(2));
+    let fs = require('fs');
+    let file = argv.file;
+    let path = require('path');
+
+    //region -- compile render function --
+    let Handlebars = require("handlebars");
+
+    Handlebars.registerHelper('json', function(context) {
+        let string = JSON.stringify(context);
+        return string.split(',').join(', ');
+    });
+
+    Handlebars.registerHelper('displayType', function(property) {
+        if (property.type == 'array' || property.type == 'Array') {
+            let type = property.items.type.toLowerCase;
+            if (type === 'string' || type === 'number'
+                ||type === 'date' || type ==='boolean') {
+                return `${property.items.type}[]`;
+            }
+            else {
+                return new
+                    Handlebars.SafeString(`<a href="${property.items.type}.html">${property.items.type}</a>[]`);
+            }
+        }
+        else {
+            return property.type;
+        }
+    });
+
+    let template = path.join(__dirname, 'JsonSchema.hbs');
+    let modelTemplate: string;
+    if (fs.existsSync(template)) {
+        modelTemplate = fs.readFileSync(template);
+    }
+    else {
+        modelTemplate = '';
+    }
+    let render = Handlebars.compile(modelTemplate.toString());
+    //endregion
+
+    if (file) {
+        genHTMLfile(render, file);
+    }
+
+    let dir = argv.dir;
+    fs.readdir(dir, function(err, items) {
+        for (let i=0; i<items.length; i++) {
+            let filePath = path.join(dir, items[i]);
+            genHTMLfile(render, filePath);
+        }
+    });
+
+    function genHTMLfile(render, file) {
+        let fs = require('fs');
+        let path = require('path');
+        if (path.extname(file) != '.json') {
+            console.error('You must choose model json file not ' + path.extname(file));
+            process.exit();
+        }
+
+        let schemaConfig = require(path.join('..', file));
+
+        let content = render(schemaConfig);
+
+        schemaConfig.gendate = new Date();
+
+        fs.writeFile(path.join(__dirname, '../docs/', schemaConfig.name + ".html"), content, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log(`HTML schema documents for ${schemaConfig.name} has been generated!`);
+        });
+    }
+})();
--- commands/ModelGenerator.ts
+++ commands/ModelGenerator.ts
@@ -2,82 +2,87 @@
  * Created by Piggat on 10/18/2016.
  */
 //node commands/ModelGenerator.js --file Logistics/
+(() => {
+    let argv = require('minimist')(process.argv.slice(2));
+    let fs = require('fs');
+    let file = argv.file;
+    let path = require('path');
 
-let argv = require('minimist')(process.argv.slice(2));
-let fs = require('fs');
-let file = argv.file;
-let path = require('path');
-
-if (path.extname(file) != '.json') {
-    console.error('You must choose model json file not ' + path.extname(file));
-    process.exit();
-}
-
-let modelConfig = require(path.join('..',file) );
-let dir = path.dirname(file);
-
-let modelName = modelConfig.name;
-let properties = modelConfig.properties;
-let relations = modelConfig.relations;
-
-//region -- get prototype function --
-let tsFile = file.replace('.json', '.ts');
-let tsContent:string;
-if (fs.existsSync(tsFile)) {
-    tsContent = fs.readFileSync(tsFile);
-}
-else {
-    tsContent = '';
-}
-//endregion
-
-function convertType(type) {
-    switch (type) {
-        case 'date': return 'Date';
-        case 'array': return 'any[]';
-        case 'Number': return 'number';
-        default: return type;
-    }
-}
-
-let Handlebars = require("handlebars");
-
-let model:any = {};
-model.name = modelName;
-//region -- Properties --
-let modelProperties = [];
-for (let propertyName in properties) {
-    if (propertyName == 'id') continue;
-    let property = properties[propertyName];
-
-    let modelProperty = {
-        name: propertyName,
-        type: convertType(property.type)
-    };
-    modelProperties.push(modelProperty);
-}
-model.properties = modelProperties;
-//endregion
-model.relations = relations;
-
-//region -- get model template --
-let template = path.join(__dirname,'ModelTemplate.hbs');
-let modelTemplate:string;
-if (fs.existsSync(template)) {
-    modelTemplate = fs.readFileSync(template);
-}
-else {
-    modelTemplate = '';
-}
-//endregion
-
-let render = Handlebars.compile(modelTemplate.toString());
-let content = render(model);
-
-fs.writeFile(path.join(dir, 'Interfaces', modelName + ".d.ts"), content, function(err) {
-    if(err) {
-        return console.log(err);
+    if (path.extname(file) != '.json') {
+        console.error('You must choose model json file not ' + path.extname(file));
+        process.exit();
     }
 
-    console.log(`The model instance type definition for ${modelName} has been generated!`);
-});
+    let modelConfig = require(path.join('..', file));
+    let dir = path.dirname(file);
+
+    let modelName = modelConfig.name;
+    let properties = modelConfig.properties;
+    let relations = modelConfig.relations;
+
+    //region -- get prototype function --
+    let tsFile = file.replace('.json', '.ts');
+    let tsContent: string;
+    if (fs.existsSync(tsFile)) {
+        tsContent = fs.readFileSync(tsFile);
+    }
+    else {
+        tsContent = '';
+    }
+    //endregion
+
+    function convertType(type) {
+        switch (type) {
+            case 'date':
+                return 'Date';
+            case 'array':
+                return 'any[]';
+            case 'Number':
+                return 'number';
+            default:
+                return type;
+        }
+    }
+
+    let Handlebars = require("handlebars");
+
+    let model: any = {};
+    model.name = modelName;
+    //region -- Properties --
+    let modelProperties = [];
+    for (let propertyName in properties) {
+        if (propertyName == 'id') continue;
+        let property = properties[propertyName];
+
+        let modelProperty = {
+            name: propertyName,
+            type: convertType(property.type)
+        };
+        modelProperties.push(modelProperty);
+    }
+    model.properties = modelProperties;
+    //endregion
+    model.relations = relations;
+
+    //region -- get model template --
+    let template = path.join(__dirname, 'ModelTemplate.hbs');
+    let modelTemplate: string;
+    if (fs.existsSync(template)) {
+        modelTemplate = fs.readFileSync(template);
+    }
+    else {
+        modelTemplate = '';
+    }
+    //endregion
+
+    let render = Handlebars.compile(modelTemplate.toString());
+    let content = render(model);
+
+    fs.writeFile(path.join(dir, 'Interfaces', modelName + ".d.ts"), content, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log(`The model instance type definition for ${modelName} has been generated!`);
+    });
+})();
