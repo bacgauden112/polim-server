820b376509f80d6b7535fdb89ce3a02fb6ff0bc5
Piggat <nguyenxuankien@alimama.vn>
2017-09-18 15:58:36 +0700

    test return agent

--- server/boot/root.ts
+++ server/boot/root.ts
@@ -5,5 +5,15 @@ export = function(server) {
     // Install a `/` route that returns server status
     let router = server.loopback.Router();
     router.get('/status', server.loopback.status());
+
+    router.get('/agent', agent());
     server.use(router);
 };
+
+function agent() {
+    return function(req, res) {
+        res.send({
+            "user-agent": req.headers['user-agent']
+        });
+    };
+}
