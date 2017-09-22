40f29839c3c23442106cdd2efb98b6dbee8f97ab
quanganh <dangquanganh@velacorp.vn>
2017-09-22 13:49:16 +0700

    fix loi 204 no content

--- domains/Integration/Services/Base/BasePurchasing.ts
+++ domains/Integration/Services/Base/BasePurchasing.ts
@@ -55,7 +55,8 @@ export class BasePurchasing extends BaseIntegration implements IPurchasing {
                 },
                 response: await response.text()
             };
-            logger.error(new Error("Error response from server"), context)
+            logger.error(new Error("Error response from server"), context);
+            throw IntegrationAPIError;
         }
         else {
             return await response.json();
